1d55cf532b1e99a36ddaffe4d271cd328d4bb4c3
yuzelin <yuzelin@xiaomi.com>
2021-12-21 21:40:04 +0800

    feat: 增加坐标轴

--- src/index.ts
+++ src/index.ts
@@ -1,73 +1,17 @@
-import * as math from "mathjs";
+import { Engine } from "babylonjs";
+import * as HandData from "./data_utils";
+import createScene from "./render3D";
 
-const REFERENCE = [0, 5, 17] //基准参考系
-
-const data = [
-  [0.6300205588340759, 1.0445104837417603, 8.649211125089096e-9],
-  [0.5864482522010803, 1.0033149719238281, 0.006611919030547142],
-  [0.5583881139755249, 0.9182900190353394, -0.006401084829121828],
-  [0.5533966422080994, 0.838855504989624, -0.021055834367871284],
-  [0.5637791156768799, 0.7830133438110352, -0.03486724570393562],
-  [0.5604788661003113, 0.9178844690322876, -0.04905194044113159],
-  [0.5737835168838501, 0.7903791666030884, -0.060112129896879196],
-  [0.5852957963943481, 0.7999221682548523, -0.050489768385887146],
-  [0.5839471817016602, 0.8316378593444824, -0.04335594177246094],
-  [0.5931977033615112, 0.9300706386566162, -0.06271127611398697],
-  [0.6101372241973877, 0.7916884422302246, -0.07930155843496323],
-  [0.6199352741241455, 0.8246104121208191, -0.06420440226793289],
-  [0.617077648639679, 0.8718233108520508, -0.050849221646785736],
-  [0.6280784010887146, 0.9377634525299072, -0.07144881784915924],
-  [0.6417489051818848, 0.8150600790977478, -0.07974396646022797],
-  [0.6471017003059387, 0.8471201658248901, -0.056434616446495056],
-  [0.6431535482406616, 0.8932198882102966, -0.03965077921748161],
-  [0.6618053317070007, 0.9475839734077454, -0.07996515929698944],
-  [0.6704261898994446, 0.8552306294441223, -0.08090528845787048],
-  [0.6721728444099426, 0.8792961835861206, -0.06409379839897156],
-  [0.670359194278717, 0.9185646176338196, -0.05126398056745529]
-];
+const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
 
 
-function normVec<T extends math.Matrix | math.MathArray>(v1: T, v2: T): T {
-  var mat = math.cross(v1, v2);
-  var den = math.norm(mat);
-  return math.divide(mat, den) as T;
-}
+const engine = new Engine(canvas, true);
 
+window.addEventListener("resize", function () {
+  engine.resize();
+});
 
-function getOrthJoints(data: number[][], refer = REFERENCE): number[][] {
-  var numJoints: number = data.length;
-  // 掌心的三个关节作为基准点
-  var [p1, p2, p3]: [number[], number[], number[]] = [data[refer[0]], data[refer[1]], data[refer[2]]];
-  // 平移矩阵T
-  var T: math.Matrix = math.matrix([[1, 0, 0, -p1[0]], [0, 1, 0, -p1[1]], [0, 0, 1, -p1[2]], [0, 0, 0, 1]]);
-
-  // 计算以基准点构建的局部坐标系
-  var vec13: number[] = math.subtract(p3, p1) as number[];
-  var vy: number[] = math.divide(vec13, math.norm(vec13) as number + 1e-6) as number[];
-  var vz: number[] = normVec(math.subtract(p2, p1) as number[], vy);
-  var vx: number[] = normVec(vy, vz);
-
-  // 求将局部坐标系旋转到与世界坐标系 z 轴重合的旋转矩阵 R1
-  var uz: number[] = vz;
-  var uy: number[] = normVec(vz, [1, 0, 0]);
-  var ux: number[] = math.cross(uy, uz) as number[];
-  var R1: math.Matrix = math.matrix([ux.concat([0]), uy.concat([0]), uz.concat([0]), [0, 0, 0, 1]]);
-
-  // 求将局部坐标系绕 z 轴旋转到与世界坐标系 x,y 轴重合的旋转矩阵 R2
-  vx = math.multiply(R1, vx.concat([1])).toArray() as number[];
-  var cos: number = vx[0];
-  var sin: number = (math.cross(math.resize(vx, [3]) as number[], [1, 0, 0]) as number[])[2] > 0 ? -vx[1] : vx[1];
-  var R2: math.Matrix = math.matrix([[cos, -sin, 0, 0], [sin, cos, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]]);
-
-  // 合并 R2、R1、T 得到变换矩阵
-  var tran: math.Matrix = math.multiply(math.multiply(R2, R1), T);
-  // 计算变换坐标系后的关节点坐标
-  var forthCol: number[] = math.ones(numJoints, 1) as number[];
-  var joints: math.Matrix = math.transpose(math.concat(data, forthCol, 1)) as math.Matrix;
-  var orthJoints: number[][] = math.transpose(math.multiply(tran, joints)).toArray() as number[][];
-
-  return orthJoints;
-}
-
-var res = getOrthJoints(data);
-console.log(res);
+const scene = createScene(canvas, engine, HandData.testData);
+engine.runRenderLoop(function () {
+  scene.render();
+});
--- src/render3D.ts
+++ src/render3D.ts
@@ -1,6 +1,6 @@
 // import * as math from "mathjs";
 import * as BABYLON from "babylonjs";
-import { RefinedResults, getBonesLength, getPalmOrthJoints, getOrthJoints, getAxes, testData } from "./data_utils";
+import { RefinedResults, getBonesLength, getPalmOrthJoints, getAxes, getOrthJoints } from "./data_utils";
 
 const JOINTS_SIZE = [2.5, 2, 1.5, 1.2, 1, 1.4, 1, 1, 1, 1.5, 1, 1, 1, 1.4, 1, 1, 1, 1.3, 1, 1, 1]; // 关节尺寸
 const PALM_JOINTS = [0, 5, 9, 13, 17]; // 手掌关节，0为手腕
@@ -12,70 +12,65 @@ const CONNECTIONS = [ // 活动关节的连接信息
   [13, 14], [14, 15], [15, 16],
   [17, 18], [18, 19], [19, 20]
 ];
+const PALM_SCALE = 5;
+const BONES_SCALE = 10;
+
 
 class HandModel {
-  protected _jointsSize: number[] = JOINTS_SIZE;
-  protected _bonesLength: number[] = new Array(21).fill(3);
-  protected _conections: number[][] = CONNECTIONS;
-  protected _referJoints: number[] = REFERENCE;
-  protected _palmJoints: number[] = PALM_JOINTS;
-  protected _palmsOrthLandmarks: number[][][]; // todo: 给定一个初始值
+  private _jointsSize: number[] = JOINTS_SIZE;
+  private _conections: number[][] = CONNECTIONS;
+  private _referJoints: number[] = REFERENCE;
+  private _palmJoints: number[] = PALM_JOINTS;
+  private _palmsOrthLandmarks: number[][][];
+  private _bonesLength: number[];
   joints: BABYLON.Mesh[];
+  scene: BABYLON.Scene;
 
   /**
    * 创建手模型，手关节由小球表示。
    * @param handType 'left' or 'right'
    */
-  constructor(handType: string) {
+  constructor(handType: string, scene: BABYLON.Scene) {
     // 定义关节模型
     this.joints = Array(21).fill({}).map((_, i) => {
-      return BABYLON.MeshBuilder.CreateSphere(`joint${i}`, { diameter: this._jointsSize[i] / 30 });
+      return BABYLON.MeshBuilder.CreateSphere(`joint${i}`, { diameter: this._jointsSize[i] / 3 });
     });
-    // this.initPalmPosition(handType === 'right');
-  }
-
-  set jointsSize(newSize: number[]) {
-    this._jointsSize = newSize;
-  }
-
-  set bonesLength(newLength: number[]) {
-    this._bonesLength = newLength;
-  }
-
-  set palmOrthLandmarks(newLandmarks: number[][][]) {
-    this._palmsOrthLandmarks = newLandmarks;
+    this.setPalmPosition(handType === 'right');
+    this.scene = scene;
+    this.showAxis(8, new BABYLON.Vector3(0, 0, 0), new BABYLON.Vector3(1, 0, 0), new BABYLON.Vector3(0, 1, 0), new BABYLON.Vector3(0, 0, 1));
   }
 
   /**
    * 更新各关节带点坐标
-   * @param landmarks 
+   * @param landmarks
    */
-  // updatePosition(landmarks: number[][]) {
-  //   // 掌心的三个关节作为基准点
-  //   var [p1, p2, p3] = [...this._referJoints.map((joint) => landmarks[joint])];
-  //   // 局部坐标系的坐标轴向量
-  //   var axes = getAxes(p1, p2, p3).map((axis) => new BABYLON.Vector3(...axis));
-  //   // 将手掌关节作为整体进行平移和旋转
-  //   this.joints[0].rotation = BABYLON.Vector3.RotationFromAxis(axes[0], axes[1], axes[2]);
-  //   this.joints[0].position = new BABYLON.Vector3(...p1);
-  //   // 确定手掌关节后再确定手指关节坐标，通过关节1和方向向量推断出相邻关节2的坐标
-  //   this._conections.map((joints, index) => {
-  //     let vec1 = new BABYLON.Vector3(...landmarks[joints[0]]);
-  //     let vec2 = new BABYLON.Vector3(...landmarks[joints[1]]);
-  //     let offset = vec2.subtract(vec1).normalize().scale(this._bonesLength[index]);
-  //     this.joints[joints[1]].position = offset.add(this.joints[joints[0]].position);
-  //   })
-  // }
-  updatePosition(_landmarks: number[][]) {
-    let landmarks = getOrthJoints(_landmarks);
-    this.joints.map((joint, index) => { joint.position = new BABYLON.Vector3(landmarks[index][0], landmarks[index][1], landmarks[index][2]); })
+  updatePosition(landmarks: number[][]) {
+    // 掌心的三个关节作为基准点
+    var [p1, p2, p3] = [...this._referJoints.map((joint) => landmarks[joint])];
+    // 局部坐标系的坐标轴向量
+    var axes = getAxes(p1, p2, p3).map((axis) => new BABYLON.Vector3(...axis));
+    // 将手掌关节作为整体进行平移和旋转
+    this.joints[0].rotation = BABYLON.Vector3.RotationFromAxis(axes[0], axes[1], axes[2]);
+    this.joints[0].position = new BABYLON.Vector3(...p1);
+    // 确定手掌关节后再确定手指关节坐标，通过关节1和方向向量推断出相邻关节2的坐标
+    // this._conections.map((joints, index) => {
+    //   let vec1 = new BABYLON.Vector3(...landmarks[joints[0]]);
+    //   let vec2 = new BABYLON.Vector3(...landmarks[joints[1]]);
+    //   let offset = vec2.subtract(vec1).normalize().scale(this._bonesLength[index]);
+    //   this.joints[joints[1]].position = this.joints[joints[0]].position.add(offset);
+    // })
   }
+
+  // updatePosition(_landmarks: number[][]) {
+  //   let landmarks = getOrthJoints(_landmarks);
+  //   this.joints.map((joint, index) => { joint.position = new BABYLON.Vector3(landmarks[index][0], landmarks[index][1], landmarks[index][2]); });
+  // }
 
   /**
    * 设置手掌关节的相对坐标（以手腕为原点的相对坐标系），实例化时调用。
    * @param 是否为右手
    */
-  initPalmPosition(isRight: boolean) {
+  setPalmPosition(isRight: boolean) {
     // 将手腕设为手掌关节的父节点
     for (let i of this._palmJoints.slice(1)) {
       this.joints[0].addChild(this.joints[i]);
@@ -86,6 +81,23 @@ class HandModel {
       this.joints[v].position = new BABYLON.Vector3(...palmOrthLandmarks[i]);
     });
   }
+
+  set palmsOrthLandmarks(landmarks: number[][][]) {
+    this._palmsOrthLandmarks = landmarks;
+  }
+
+  set bonesLength(length: number[]) {
+    this._bonesLength = length;
+  }
+
+  showAxis(size: number, origin: BABYLON.Vector3, x: BABYLON.Vector3, y: BABYLON.Vector3, z: BABYLON.Vector3) {
+    var axisX = BABYLON.Mesh.CreateLines("axisX", [origin, x.scale(size)], this.scene);
+    axisX.color = new BABYLON.Color3(1, 0, 0);
+    var axisY = BABYLON.Mesh.CreateLines("axisY", [origin, y.scale(size)], this.scene);
+    axisY.color = new BABYLON.Color3(0, 1, 0);
+    var axisZ = BABYLON.Mesh.CreateLines("axisZ", [origin, z.scale(size)], this.scene);
+    axisZ.color = new BABYLON.Color3(0, 0, 1);
+  };
 }
 
 
@@ -112,24 +124,23 @@ const createScene = function (canvas: HTMLCanvasElement, engine: BABYLON.Engine,
       return;
     }
 
-    // 第一次捕捉到手时进行初始化
+    // 第一次捕捉到手时初始化手掌坐标和手骨长度
     if (!leftHand && !rightHand) {
       let landmarks = results.leftLandmarks || results.rightLandmarks;
-      HandModel.prototype.jointsSize = JOINTS_SIZE;
-      HandModel.prototype.bonesLength = getBonesLength(landmarks, CONNECTIONS);
-      HandModel.prototype.palmOrthLandmarks = getPalmOrthJoints(landmarks, PALMJOINTS, results.isLeftHandCaptured, 10);
+      HandModel.prototype.palmsOrthLandmarks = getPalmOrthJoints(landmarks, PALM_JOINTS, results.isLeftHandCaptured, PALM_SCALE);
+      HandModel.prototype.bonesLength = getBonesLength(landmarks, CONNECTIONS, BONES_SCALE);
     }
 
     if (results.isLeftHandCaptured) {
       if (!leftHand) {
-        leftHand = new HandModel('left');
+        leftHand = new HandModel('left', scene);
       }
       leftHand.updatePosition(results.leftLandmarks);
     }
 
     if (results.isRightHandCaptured) {
       if (!rightHand) {
-        rightHand = new HandModel('right');
+        rightHand = new HandModel('right', scene);
       }
       rightHand.updatePosition(results.rightLandmarks);
     }
